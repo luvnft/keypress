7f851e6cca3b1485182a2bf72d5d3631c307d98b
luv nft <info@blkluv.org>
2023-12-17 09:41:41 -0500

    updated

--- server/middleware/subdomain.ts
+++ server/middleware/subdomain.ts
@@ -1,12 +1,12 @@
 export default defineEventHandler(({ req, res, context }) => {
-  const hostname = req.headers.host || "keypress.blog"
+  const hostname = req.headers.host || "arvrtise.link"
 
-  const mainDomain = ["localhost:3000", "keypress.blog"]
+  const mainDomain = ["localhost:3000", "arvrtise.link"]
 
   if (!mainDomain.includes(hostname)) {
     const currentHost =
       process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
-        ? hostname.replace(`.keypress.blog`, "")
+        ? hostname.replace(`.arvrtise.link`, "")
         : hostname.replace(`.localhost:3000`, "")
 
     console.log({ currentHost })
